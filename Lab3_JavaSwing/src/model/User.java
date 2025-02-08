7f4f8ec6c9fc3c6660d9fb04b2235b8a9a09a077
Soumya Singh <singh.soumy@northeastern.edu>
2025-02-08 11:32:33 -0500

    updating
    updating and reoraganizing files

---
+++ Lab3_JavaSwing/src/model/User.java
@@ -0,0 +1,46 @@
+package model;
+
+public class User {
+    //data members
+    private String name; private String college;
+    private int age;
+    private String hobby;
+    // methods
+    public String getName() {
+    return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getCollege() {
+    return college;
+    }
+    
+    public void setCollege(String college) {
+    this.college = college;
+    }
+    
+    public int getAgel() {
+    return age;
+    }
+    
+    public void setAge(int age) {
+    this.age = age;
+    }
+    
+    public String getHobby () {
+    return hobby;
+    }
+    
+    public void setHobby(String hobby){
+        this.hobby = hobby;
+    }
+    
+    @Override
+    public String toString() {
+    return "Userf" + "name=" + name +", college=" + college +", age=" + age +", hobby=" + hobby + '}';
+
+    }
+}
