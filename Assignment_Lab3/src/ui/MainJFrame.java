a84e0b531b6758784a4e2831b1de627783f8307e
Soumya Singh <singh.soumy@northeastern.edu>
2025-02-11 16:35:55 -0500

    lab3 assignment update
    adding lab 3 assignment update

--- Assignment_Lab3/src/ui/MainJFrame.java
+++ Assignment_Lab3/src/ui/MainJFrame.java
@@ -5,6 +5,11 @@
 package ui;
 
 import java.awt.HeadlessException;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import model.User;
 
@@ -17,10 +22,12 @@ public class MainJFrame extends javax.swing.JFrame {
     /**
      * Creates new form MainJFrame
      */
+    ImageIcon picture;
+    
     public MainJFrame() {
         initComponents();
+        
     }
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -48,8 +55,9 @@ public class MainJFrame extends javax.swing.JFrame {
         emailField = new javax.swing.JTextField();
         phoneLabel = new javax.swing.JLabel();
         phoneField = new javax.swing.JTextField();
-        photoLabel = new javax.swing.JLabel();
-        photoField = new javax.swing.JTextField();
+        imgLabel = new javax.swing.JLabel();
+        imageLabel = new javax.swing.JLabel();
+        imageButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -127,11 +135,12 @@ public class MainJFrame extends javax.swing.JFrame {
             }
         });
 
-        photoLabel.setText("Photo");
+        imgLabel.setText("Upload Photo");
 
-        photoField.addActionListener(new java.awt.event.ActionListener() {
+        imageButton.setText("Submit Image");
+        imageButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                photoFieldActionPerformed(evt);
+                imageButtonActionPerformed(evt);
             }
         });
 
@@ -144,10 +153,7 @@ public class MainJFrame extends javax.swing.JFrame {
                 .addComponent(titleLabel)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(submitButton))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(19, 19, 19)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
@@ -161,18 +167,23 @@ public class MainJFrame extends javax.swing.JFrame {
                             .addComponent(hobbiesField)
                             .addComponent(genderComboBox, 0, 148, Short.MAX_VALUE)
                             .addComponent(fnameField))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lnameLabel, javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(ageLabel, javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(phoneLabel, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(photoLabel, javax.swing.GroupLayout.Alignment.TRAILING))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(photoField, javax.swing.GroupLayout.DEFAULT_SIZE, 108, Short.MAX_VALUE)
-                            .addComponent(phoneField)
-                            .addComponent(ageField)
-                            .addComponent(lnameField))))
+                            .addComponent(imgLabel, javax.swing.GroupLayout.Alignment.TRAILING))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(submitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(48, 48, 48)))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(phoneField)
+                    .addComponent(ageField)
+                    .addComponent(lnameField)
+                    .addComponent(imageLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(imageButton))
                 .addGap(50, 50, 50))
         );
         jPanel1Layout.setVerticalGroup(
@@ -198,28 +209,35 @@ public class MainJFrame extends javax.swing.JFrame {
                     .addComponent(emailLabel)
                     .addComponent(phoneField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(phoneLabel))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(hobbiesLabel)
-                    .addComponent(hobbiesField, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(photoLabel)
-                    .addComponent(photoField, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
-                .addComponent(submitButton)
-                .addGap(42, 42, 42))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(hobbiesLabel)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(30, 30, 30)
+                                .addComponent(imageLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(hobbiesField, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(imgLabel)
+                                .addComponent(imageButton)))
+                        .addGap(73, 73, 73)
+                        .addComponent(submitButton)
+                        .addGap(27, 27, 27))))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
@@ -256,7 +274,7 @@ public class MainJFrame extends javax.swing.JFrame {
             if(userObject.getFName().isBlank()){
                 JOptionPane.showMessageDialog(rootPane, "Please enter your first name", "Oops!", HEIGHT);
             } else {
-                JOptionPane.showMessageDialog(rootPane, userObject, "Success!", HEIGHT);
+                JOptionPane.showMessageDialog(rootPane, userObject, "Success!", HEIGHT, this.picture);
             // "Welcome " + <name> + " to "+ <college>
             } 
             
@@ -265,7 +283,7 @@ public class MainJFrame extends javax.swing.JFrame {
             System.out.println(ne.getMessage());
             ne.printStackTrace();
         } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(rootPane, "Please enter your age correctly ", "Oops!", HEIGHT);
+            JOptionPane.showMessageDialog(rootPane, "Please enter your age or phone correctly ", "Oops!", HEIGHT);
         } catch (Exception e){
             JOptionPane.showMessageDialog(rootPane, "Oops, something went wrong! ", "Failed!", HEIGHT);
         }
@@ -296,9 +314,25 @@ public class MainJFrame extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_phoneFieldActionPerformed
 
-    private void photoFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_photoFieldActionPerformed
+    private void imageButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_imageButtonActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_photoFieldActionPerformed
+    JFileChooser file = new JFileChooser();
+    if (file.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+    try {
+    BufferedImage img = ImageIO.read(file.getSelectedFile());
+    Image edited_image = img.getScaledInstance(60, 80, Image.SCALE_SMOOTH);
+    if (edited_image != null) {
+        ImageIcon picture = new ImageIcon(edited_image);
+        imageLabel.setIcon(picture);
+    //this.userInfo.setPic(new ImageIcon(edited_image));
+    }
+    } catch (Exception ex) {
+    JOptionPane.showMessageDialog(this, "Please upload image correctly correctly.", "Error - Incorrect image", JOptionPane.ERROR_MESSAGE);
+    ex.printStackTrace();
+    }
+    }
+
+    }//GEN-LAST:event_imageButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -329,10 +363,8 @@ public class MainJFrame extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MainJFrame().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new MainJFrame().setVisible(true);
         });
     }
 
@@ -348,13 +380,14 @@ public class MainJFrame extends javax.swing.JFrame {
     private javax.swing.JLabel genderLabel;
     private javax.swing.JTextField hobbiesField;
     private javax.swing.JLabel hobbiesLabel;
+    private javax.swing.JButton imageButton;
+    private javax.swing.JLabel imageLabel;
+    private javax.swing.JLabel imgLabel;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField lnameField;
     private javax.swing.JLabel lnameLabel;
     private javax.swing.JTextField phoneField;
     private javax.swing.JLabel phoneLabel;
-    private javax.swing.JTextField photoField;
-    private javax.swing.JLabel photoLabel;
     private javax.swing.JButton submitButton;
     private javax.swing.JLabel titleLabel;
     // End of variables declaration//GEN-END:variables
