559e6b6f51c6187cd151f4462f4e5a2eeb3b97fc
Soumya Singh <singh.soumy@northeastern.edu>
2025-02-12 16:08:37 -0500

    adding lab4

---
+++ Lab4_Navigations/src/model/User.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author soumyasingh
+ */
+public class User {
+   //data members
+    private String name;
+    private String college;
+    private int age;
+    private String hobby;
+       
+    //methods
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "name=" + name + ", college=" + college + ", age=" + age + ", hobby=" + hobby + '}';
+    }
+    
+}
