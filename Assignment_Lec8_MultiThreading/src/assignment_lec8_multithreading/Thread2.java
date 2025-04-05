72418c24f7c22c76e32d933f371a50a520b4a29e
Soumya Singh <singh.soumy@northeastern.edu>
2025-04-04 22:10:17 -0400

    Update Thread2.java

--- Assignment_Lec8_MultiThreading/src/assignment_lec8_multithreading/Thread2.java
+++ Assignment_Lec8_MultiThreading/src/assignment_lec8_multithreading/Thread2.java
@@ -13,19 +13,19 @@ public class Thread2 extends Thread {
     String[] tones = {"re.wav", "fa.wav","la.wav", "do-octave.wav"};
     FilePlayer fp = new FilePlayer();
     
-        @Override
+    @Override
     public void run(){
         for (String tone: tones){
             try{
                 if(tone.equals("do-octave.wav")){
                     Thread.sleep(1500);
                     fp.play("/Users/soumyasingh/Documents/Northeastern University/INFO5100/soumya-singh/Assignment_Lec8_MultiThreading/src/Sounds/do-octave.wav");
-                            Thread.sleep(500);
+                    Thread.sleep(500);
                 }else{
                     Thread.sleep(500);
-                fp.play("/Users/soumyasingh/Documents/Northeastern University/INFO5100/soumya-singh/Assignment_Lec8_MultiThreading/src/Sounds/re.wav");
-                System.out.println("Thread2: "+tone);
-                Thread.sleep(500);
+                    fp.play("/Users/soumyasingh/Documents/Northeastern University/INFO5100/soumya-singh/Assignment_Lec8_MultiThreading/src/Sounds/re.wav");
+                    System.out.println("Thread2: "+tone);
+                    Thread.sleep(500);
                 }
             } catch(Exception e){
                 e.printStackTrace();
